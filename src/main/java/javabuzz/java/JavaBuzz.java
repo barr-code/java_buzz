5e2f4c4ef2484011b6e4cf7afe420aee60bdf535
Victoria Barr <vb329@georgetown.edu>
2014-11-18 11:50:31 +0000

    JavaBuzz knows when nums are/aren't divisible by 3 and 5

---
+++ src/main/java/javabuzz/java/JavaBuzz.java
@@ -0,0 +1,11 @@
+package javabuzz.java;
+
+public class JavaBuzz {
+    public boolean isDivisibleByThree(int number) {
+        return number % 3 == 0;
+    }
+
+    public boolean isDivisibleByFive(int number) {
+        return number % 5 == 0;
+    }
+}
---
+++ src/test/java/JavaBuzzTest.java
@@ -0,0 +1,31 @@
+import javabuzz.java.JavaBuzz;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class JavaBuzzTest {
+    @Test
+    public void isDivisibleByThree(){
+        JavaBuzz javabuzz = new JavaBuzz();
+        assertTrue(javabuzz.isDivisibleByThree(3));
+    }
+
+    @Test
+    public void isNotDivisibleByThree(){
+        JavaBuzz javabuzz = new JavaBuzz();
+        assertFalse(javabuzz.isDivisibleByThree(1));
+    }
+
+    @Test
+    public void isDivisibleByFive(){
+        JavaBuzz javabuzz = new JavaBuzz();
+        assertTrue(javabuzz.isDivisibleByFive(5));
+    }
+
+    @Test
+    public void isNotDivisibleByFive(){
+        JavaBuzz javabuzz = new JavaBuzz();
+        assertFalse(javabuzz.isDivisibleByFive(1));
+    }
+}
